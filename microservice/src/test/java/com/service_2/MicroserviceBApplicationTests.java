67e083e17b34582e3981c88eb85a6d3b52312fb4
onkarsuryawanshi <onkar.suryawanshi@cognologix.com>
2022-11-06 17:08:05 +0530

    service-2

---
+++ microservice/src/test/java/com/service_2/MicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
